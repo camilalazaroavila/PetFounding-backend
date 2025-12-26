af7ce615d166d2a06d146f669092514b86de991d
MatiasKV0 <matiasvlln@gmail.com>
2025-12-25 22:27:04 -0300

    Add SolicitudDuplicadaException and enhance error handling in ErrorManager

---
+++ backend/src/main/java/com/petFounding/exception/SolicitudDuplicadaException.java
@@ -0,0 +1,7 @@
+package com.petFounding.exception;
+
+public class SolicitudDuplicadaException extends RuntimeException {
+    public SolicitudDuplicadaException(String message) {
+        super(message);
+    }
+}
--- backend/src/main/java/com/petFounding/valid/UserValid.java
+++ backend/src/main/java/com/petFounding/valid/UserValid.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 
 public record UserValid(
         @NotBlank @Email String email,
-        @NotBlank @Size(min = 6) String password,
+        @NotBlank @Size(min = 6, max = 256) String password,
         @NotBlank String nombre,
         @NotBlank String apellido,
         String telefono,
